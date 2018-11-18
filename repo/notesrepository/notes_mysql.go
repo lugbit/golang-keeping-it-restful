96921d73e09d9f21f47e307c0bde65062ff80cee
Marck M <Marck527@gmail.com>
2018-11-18 17:25:42 +1100

    All functions now return an error

--- repo/notesrepository/notes_mysql.go
+++ repo/notesrepository/notes_mysql.go
@@ -3,6 +3,7 @@ package notesrepository
 
 import (
 	"database/sql"
+	"errors"
 	"log"
 
 	"../../models"
@@ -11,9 +12,8 @@ import (
 // NotesRepository struct for initialization
 type NotesRepository struct{}
 
-// GetNotes interacts with the db directly to get all notes and returns
-// a slice of notes
-func (n NotesRepository) GetNotes(db *sql.DB, note models.Note, notes []models.Note) []models.Note {
+// GetNotes queries the database for all notes and returns a slice of notes and an error.
+func (n NotesRepository) GetNotes(db *sql.DB, note models.Note, notes []models.Note) ([]models.Note, error) {
 	sqlStmt := `SELECT
 					fldID,
 					fldCreatedAt,
@@ -38,8 +38,12 @@ func (n NotesRepository) GetNotes(db *sql.DB, note models.Note, notes []models.N
 	}
 	defer rows.Close()
 
+	// Row counter to find out if the query returned a row
+	rowCount := 0
 	// Loop through each row
 	for rows.Next() {
+		// Increment row
+		rowCount++
 		// Scan each row to note struct
 		err := rows.Scan(&note.ID, &note.CreatedAt, &note.UpdatedAt, &note.Title, &note.Note, &note.Colour, &note.Archived)
 		if err != nil {
@@ -50,6 +54,12 @@ func (n NotesRepository) GetNotes(db *sql.DB, note models.Note, notes []models.N
 		notes = append(notes, note)
 	}
 
+	// No rows found
+	if rowCount == 0 {
+		// Return error
+		return []models.Note{}, errors.New("No rows found")
+	}
+
 	// Check for errors during row scan
 	err = rows.Err()
 	if err != nil {
@@ -57,11 +67,11 @@ func (n NotesRepository) GetNotes(db *sql.DB, note models.Note, notes []models.N
 	}
 
 	// Return slice of notes
-	return notes
+	return notes, nil
 }
 
-// GetNote serches the db for a note based on argument ID and returns a models.Note
-func (n NotesRepository) GetNote(db *sql.DB, note models.Note, id int) models.Note {
+// GetNote queries the database for a particular note and returns a note and an error.
+func (n NotesRepository) GetNote(db *sql.DB, note models.Note, id int) (models.Note, error) {
 	sqlStmt := `SELECT
 					fldID,
 					fldCreatedAt,
@@ -83,20 +93,21 @@ func (n NotesRepository) GetNote(db *sql.DB, note models.Note, id int) models.No
 	// Return row and scan columns to passed note
 	err = stmt.QueryRow(id).Scan(&note.ID, &note.CreatedAt, &note.UpdatedAt, &note.Title, &note.Note, &note.Colour, &note.Archived)
 	if err != nil {
-		// No rows found
+		// No row found
 		if err == sql.ErrNoRows {
-			// Return empty note
-			return models.Note{}
+			// Return empty note and error
+			return models.Note{}, errors.New("Row not found")
 		}
 		// Something else went wrong
 		log.Fatalln(err)
 	}
 
-	return note
+	return note, nil
 }
 
 // AddNote inserts a new note in the database and returns the last insert ID
-func (n NotesRepository) AddNote(db *sql.DB, note models.Note) int {
+// and an error.
+func (n NotesRepository) AddNote(db *sql.DB, note models.Note) (int, error) {
 	sqlStmt := `INSERT INTO tblNotes(fldTitle, fldNote) 
 				VALUES 
 					(?, ?)`
@@ -114,6 +125,16 @@ func (n NotesRepository) AddNote(db *sql.DB, note models.Note) int {
 		log.Fatalln(err)
 	}
 
+	// Check if any rows was affected
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Rows affected is zero, insert failed
+	if rowsAffected == 0 {
+		return -1, errors.New("No rows affected")
+	}
+
 	// Get last insert ID
 	id, err := res.LastInsertId()
 	if err != nil {
@@ -121,11 +142,12 @@ func (n NotesRepository) AddNote(db *sql.DB, note models.Note) int {
 	}
 
 	// Return last insert ID
-	return int(id)
+	return int(id), nil
 }
 
-// UpdateNote updates an existing note entry in the database
-func (n NotesRepository) UpdateNote(db *sql.DB, note models.Note) int {
+// UpdateNote updates an existing note in the database and returns the number of
+// rows affected and an error.
+func (n NotesRepository) UpdateNote(db *sql.DB, note models.Note) (int, error) {
 	sqlStmt := `UPDATE tblNotes
 					SET fldTitle = ?, fldNote = ?, fldColour = ?, fldArchived = ?
 				WHERE fldID = ?;`
@@ -148,13 +170,18 @@ func (n NotesRepository) UpdateNote(db *sql.DB, note models.Note) int {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	// If number of rows affected is zero, nothing was updated
+	if rowsAffected == 0 {
+		return -1, errors.New("No rows affected")
+	}
 
 	// Return number of rows affected
-	return int(rowsAffected)
+	return int(rowsAffected), nil
 }
 
-// DeleteNote deletes an existing note entry in the database
-func (n NotesRepository) DeleteNote(db *sql.DB, id int) int {
+// DeleteNote deletes an existing note in the database and returns the number of
+// rows affected and an error.
+func (n NotesRepository) DeleteNote(db *sql.DB, id int) (int, error) {
 	sqlStmt := `DELETE FROM tblNotes
 				WHERE fldID = ?`
 
@@ -176,7 +203,11 @@ func (n NotesRepository) DeleteNote(db *sql.DB, id int) int {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	// If number of rows affected is zero, nothing was deleted
+	if rowsAffected == 0 {
+		return -1, errors.New("No rows affected")
+	}
 
 	// Return number of rows affected
-	return int(rowsAffected)
+	return int(rowsAffected), nil
 }
