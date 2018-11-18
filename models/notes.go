d5e430a5278f6dc5333b329615e8c4ca9901973d
Marck M <Marck527@gmail.com>
2018-11-18 11:34:51 +1100

    Data models

---
+++ models/notes.go
@@ -0,0 +1,13 @@
+// Package models stores data models
+package models
+
+// Note model with JSON tags
+type Note struct {
+	ID        int    `json:"id"`
+	CreatedAt string `json:"created"`
+	UpdatedAt string `json:"updated"`
+	Title     string `json:"title"`
+	Note      string `json:"note"`
+	Colour    string `json:"colour"`
+	Archived  bool   `json:"archived"`
+}
