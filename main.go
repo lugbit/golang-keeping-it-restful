27ba3f9352a4b09b826d44536be2342fd8be8b1f
Marck M <Marck527@gmail.com>
2018-11-18 11:34:24 +1100

    Main file

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"./controllers"
+	"./drivers"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	gotenv "github.com/subosito/gotenv"
+)
+
+// Gobal DB handler variable
+var db *sql.DB
+
+func init() {
+	// Load environment variables on startup
+	gotenv.Load()
+}
+
+func main() {
+	// Open DB handler and defer close
+	db = drivers.ConnectDB()
+	defer db.Close()
+
+	// New controller
+	controller := controllers.Controller{}
+
+	// Gorilla mux
+	r := mux.NewRouter()
+
+	// Routes
+	r.HandleFunc("/notes", controller.GetNotes(db)).Methods("GET")
+	r.HandleFunc("/notes/{id}", controller.GetNote(db)).Methods("GET")
+	r.HandleFunc("/notes", controller.AddNote(db)).Methods("POST")
+	r.HandleFunc("/notes", controller.UpdateNote(db)).Methods("PUT")
+	r.HandleFunc("/notes/{id}", controller.DeleteNote(db)).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(os.Getenv("SERVER_PORT"), r))
+}
