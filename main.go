79fcb1c268b2e26d3e918ad52e2f5a7f2d8384c0
Marck M <Marck527@gmail.com>
2018-11-25 19:50:54 +1100

    Protected all note related routes.

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"./controllers"
 	"./drivers"
-	mw "./middlewares"
+	"./middlewares"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	gotenv "github.com/subosito/gotenv"
@@ -19,7 +20,10 @@ var db *sql.DB
 
 func init() {
 	// Load environment variables on startup
-	gotenv.Load()
+	err := gotenv.Load()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
 
 func main() {
@@ -27,19 +31,25 @@ func main() {
 	db = drivers.ConnectDB()
 	defer db.Close()
 
-	// New controller
+	// Initialize controllers
 	controller := controllers.Controller{}
+	// Initialize middlewares
+	middleware := middlewares.Middleware{}
 
 	// Gorilla mux
 	r := mux.NewRouter()
 
 	// Routes
-	r.Handle("/notes", mw.VerifyToken(controller.GetNotes(db))).Methods("GET")
-	r.HandleFunc("/notes/{id}", controller.GetNote(db)).Methods("GET")
-	r.HandleFunc("/notes", controller.AddNote(db)).Methods("POST")
-	r.HandleFunc("/notes", controller.UpdateNote(db)).Methods("PUT")
-	r.HandleFunc("/notes/{id}", controller.DeleteNote(db)).Methods("DELETE")
+
+	// Open routes
 	r.Handle("/users/login", controller.LoginUser(db)).Methods("POST")
+
+	// Protected routes
+	r.Handle("/notes", middleware.Authorized(controller.GetNotes(db))).Methods("GET")
+	r.Handle("/notes/{id}", middleware.Authorized(controller.GetNote(db))).Methods("GET")
+	r.Handle("/notes", middleware.Authorized(controller.AddNote(db))).Methods("POST")
+	r.Handle("/notes", middleware.Authorized(controller.UpdateNote(db))).Methods("PUT")
+	r.Handle("/notes/{id}", middleware.Authorized(controller.DeleteNote(db))).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(os.Getenv("SERVER_PORT"), r))
 }
