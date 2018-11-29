5e9cce06846e9823d0a7f4fef11bd50b183c9b38
Marck M <Marck527@gmail.com>
2018-11-29 20:23:20 +1100

    Moved JWT claims model from seperate file to jwt.go.

---
+++ models/jwt.go
@@ -0,0 +1,22 @@
+package models
+
+import jwt "github.com/dgrijalva/jwt-go"
+
+// UserClaims is a custom JWT claims struct which is used
+// by the jwt-go package.
+//
+// This is the 'payload' part of the generated JWT sent to
+// the user after successfully authenticating.
+type UserClaims struct {
+	// Custom claims
+	UserID    int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	// Standard claims (part of jwt-go)
+	jwt.StandardClaims
+}
+
+// JWT struct to send to user upon authenticating
+type JWT struct {
+	Token string `json:"token"`
+}
